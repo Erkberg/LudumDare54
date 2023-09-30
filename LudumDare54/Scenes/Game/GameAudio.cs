0c882aba7ca5266d026d831b47995378d0865ed2
Erkberg <erisch01@web.de>
2023-09-30 18:36:07 +0200

    prepare audio a tiny bit

---
+++ LudumDare54/Scenes/Game/GameAudio.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public partial class GameAudio : Node
+{
+    [Export] private AudioStreamPlayer soundPlayer;
+
+    public override void _Ready()
+    {
+        
+    }
+
+    public void PlayDieSound()
+    {
+
+    }
+
+    public void PlayDashSound()
+    {
+
+    }
+
+    public void PlayCoinSound()
+    {
+
+    }
+
+    public void PlaySpawnSound()
+    {
+
+    }
+}

99895789148ded0080131e58f818dcff77831d68
Erkberg <erisch01@web.de>
2023-09-30 11:37:10 +0200

    basic player dash

--- LudumDare54/Scenes/Game/GameInput.cs
+++ LudumDare54/Scenes/Game/GameInput.cs
@@ -7,6 +7,7 @@ public partial class GameInput : Node
     private const string MoveRight = "MoveRight";
     private const string MoveUp = "MoveUp";
     private const string MoveDown = "MoveDown";
+    private const string Dash = "Dash";
 
     public Vector2 GetMove()
     {
@@ -16,5 +17,20 @@ public partial class GameInput : Node
         moveDir.Y = Input.GetActionStrength(MoveDown) - Input.GetActionStrength(MoveUp);
 
         return moveDir;
+    }
+
+    public bool GetDash()
+    {
+        return Input.IsActionPressed(Dash);
+    }
+
+    public bool GetDashDown()
+    {
+        return Input.IsActionJustPressed(Dash);
+    }
+
+    public bool GetDashUp()
+    {
+        return Input.IsActionJustReleased(Dash);
     }
 }
--- LudumDare54/Scenes/Player/Player.cs
+++ LudumDare54/Scenes/Player/Player.cs
@@ -4,26 +4,60 @@ using System;
 public partial class Player : CharacterBody2D
 {
     [Export] private float moveSpeed = 160;
+    [Export] private float dashMultiplier = 4;
     [Export] private float accelerationSmoothing = 16;
+    [Export] private Timer dashTimer;
 
     private GameInput input;
+    private Vector2 currentDirection;
+    private bool isDashing;
 
     public override void _Ready()
     {
         Game.inst.refs.player = this;
         input = Game.inst.input;
+        dashTimer.Timeout += OnDashTimer;
     }
 
     public override void _Process(double delta)
     {
+        CheckDash();
         Move(delta);
     }
 
     private void Move(double delta)
     {
-        Vector2 direction = input.GetMove().Normalized();
-        Vector2 targetVelocity = direction * moveSpeed;
-        Velocity = Velocity.Lerp(targetVelocity, (float)(1 - Mathf.Exp(-delta * accelerationSmoothing)));
+        if(!isDashing)
+        {
+            currentDirection = input.GetMove().Normalized();
+            Vector2 targetVelocity = currentDirection * moveSpeed;
+            Velocity = Velocity.Lerp(targetVelocity, (float)(1 - Mathf.Exp(-delta * accelerationSmoothing)));
+        }
+        else
+        {
+            Vector2 targetVelocity = currentDirection * moveSpeed * dashMultiplier;
+            Velocity = targetVelocity;
+        }
+
         MoveAndSlide();
+    }
+
+    private void CheckDash()
+    {
+        if(!isDashing && input.GetDashDown())
+        {
+            isDashing = true;
+            dashTimer.Start();
+        }
+        else if(isDashing && input.GetDashUp())
+        {
+            isDashing = false;
+            dashTimer.Stop();
+        }
+    }
+
+    private void OnDashTimer()
+    {
+        isDashing = false;
     }
 }
